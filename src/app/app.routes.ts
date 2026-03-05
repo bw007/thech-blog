aa82310ce493ca16983a28e7a12608d65e4af54d
bw007 <mr.khasanov02@gmail.com>
2026-03-05 23:55:11 +0500

    feat: update routing to use MainLayoutComponent and add auth route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
-import { MainLayout } from './layout/main-layout/main-layout';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    loadComponent: () => import('@features/auth/auth.component').then(m => m.AuthComponent)
+  },
+  {
     path: '',
-    component: MainLayout
+    component: MainLayoutComponent
   }
 ];
