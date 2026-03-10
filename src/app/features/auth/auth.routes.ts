94efc7961808db21893a65ff250add9840210f62
bw007 <mr.khasanov02@gmail.com>
2026-03-10 23:52:33 +0500

    feat: add auth routes for sign-in, sign-up, verify-email, forgot-password, and new-password components

---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,38 @@
+import { Routes } from '@angular/router';
+
+export const authRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'sign-in'
+  },
+  {
+    path: 'full',
+    loadComponent: () => import('@layout/auth-layout/auth-full/auth-full.component').then(m => m.AuthFullComponent),
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'sign-in' },
+      {
+        path: 'sign-in',
+        loadComponent: () => import('@features/auth/sign-in/sign-in.component').then(m => m.SignInComponent),
+        data: { key: 'signin' }
+      },
+      {
+        path: 'sign-up',
+        loadComponent: () => import('@features/auth/sign-up/sign-up.component').then(m => m.SignUpComponent),
+        data: { key: 'signup' }
+      },
+    ]
+  },
+  {
+    path: 'verify-email',
+    loadComponent: () => import('@features/auth/verify-email/verify-email.component').then(m => m.VerifyEmailComponent),
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () => import('@features/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
+  },
+  {
+    path: 'new-password',
+    loadComponent: () => import('@features/auth/new-password/new-password.component').then(m => m.NewPasswordComponent),
+  }
+];
