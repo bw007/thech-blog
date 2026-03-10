e53cc47cf2799df89acaf3b684f5ecabacfa95f1
bw007 <mr.khasanov02@gmail.com>
2026-03-10 23:36:58 +0500

    fix: correct import order in MainLayoutComponent

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from "@angular/core";
-import { HeaderComponent } from "../components/header/header.component";
 import { RouterOutlet } from "@angular/router";
-import { FooterComponnet } from "../components/footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponnet } from "./footer/footer.component";
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  imports: [HeaderComponent, RouterOutlet, FooterComponnet]
+  imports: [RouterOutlet, HeaderComponent, FooterComponnet]
 })
 
 export class MainLayoutComponent {}
