b87c7ea693456db3284ed9bf21caad98ecba6e6d
bw007 <mr.khasanov02@gmail.com>
2026-03-10 23:52:08 +0500

    feat: update auth route to use AuthLayoutComponent and load auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
 export const routes: Routes = [
   {
     path: 'auth',
-    loadComponent: () => import('@features/auth/auth.component').then(m => m.AuthComponent)
+    loadComponent: () => import('@layout/auth-layout/auth-layout.component').then(m => m.AuthLayoutComponent),
+    loadChildren: () => import('@features/auth/auth.routes').then(m => m.authRoutes)
   },
   {
     path: '',
