d3f5e96ffed1a5a9c44e90ec98c866b8baf31c2c
bw007 <mr.khasanov02@gmail.com>
2026-03-11 12:59:15 +0500

    feat: update routes constants property key

--- src/app/core/constants/routes.constants.ts
+++ src/app/core/constants/routes.constants.ts
@@ -1,10 +1,11 @@
 export const routes = {
   auth: {
-    root: '/auth',
-    signIn: '/auth/full/sign-in',
-    signUp: '/auth/full/sign-up',
-    verifyEmail: '/auth/verify-email',
-    forgotPassword: '/auth/forgot-password',
+    ROOT: '/auth',
+    SIGN_IN: '/auth/full/sign-in',
+    SIGN_UP: '/auth/full/sign-up',
+    VERIFY_EMAIL: '/auth/verify-email',
+    FORGOT_PASSWORD: '/auth/forgot-password',
+    NEW_PASSWORD: '/auth/new-password',
   },
   home: '/',
 } as const;
