0050df035c0b80cd14f702b768df50a343975f9a
bw007 <mr.khasanov02@gmail.com>
2026-03-11 15:16:40 +0500

    feat: add guest guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { guestGuard } from '@features/auth/guards/guest.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadComponent: () => import('@layout/auth-layout/auth-layout.component').then(m => m.AuthLayoutComponent),
-    loadChildren: () => import('@features/auth/auth.routes').then(m => m.authRoutes)
+    loadChildren: () => import('@features/auth/auth.routes').then(m => m.authRoutes),
+    canActivate: [guestGuard]
   },
   {
     path: '',
