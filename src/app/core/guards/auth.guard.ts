81290d07a8bf42bd8d60e8d3b1d413847b95dc30
bw007 <mr.khasanov02@gmail.com>
2026-03-16 11:03:59 +0500

    feat: add auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { inject } from "@angular/core";
+import { CanActivateFn, RedirectCommand, Router } from "@angular/router";
+import { toObservable } from "@angular/core/rxjs-interop";
+import { filter, map, take } from "rxjs";
+import { AuthService } from "@core/services/auth.service";
+
+export const authGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const auth = inject(AuthService);
+
+  return toObservable(auth.isLoading).pipe(
+    filter(isLoading => !isLoading),
+    take(1),
+    map(() => {
+      if (!auth.isLoggedIn()) {
+        return new RedirectCommand(router.parseUrl('/'));
+      }
+      return true;
+    })
+  );
+}
