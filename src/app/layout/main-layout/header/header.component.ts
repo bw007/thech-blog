2f3734b825599e36f65ae5579daf8268f4b57e1c
bw007 <mr.khasanov02@gmail.com>
2026-03-16 13:20:37 +0500

    feat: add theme service

--- src/app/layout/main-layout/header/header.component.ts
+++ src/app/layout/main-layout/header/header.component.ts
@@ -4,6 +4,7 @@ import { TuiButton, TuiIcon } from '@taiga-ui/core';
 import { TuiDataList, TuiDropdown } from '@taiga-ui/core';
 import { TuiDataListDropdownManager, TuiAvatar, TuiAvatarOutline } from '@taiga-ui/kit';
 import { AuthService } from '@core/services/auth.service';
+import { ThemeService } from '@core/services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -21,11 +22,17 @@ import { AuthService } from '@core/services/auth.service';
   ]
 })
 export class HeaderComponent {
+  private theme = inject(ThemeService);
   private auth = inject(AuthService);
   protected dropdownOpen = signal(false);
   protected readonly user = this.auth.currentUser;
   protected readonly isLoggedIn = this.auth.isLoggedIn;
   protected readonly isLoading = this.auth.isLoading;
+  protected readonly darkMode = this.theme.darkMode;
+
+  themeToggle() {
+    this.theme.themeToggle()
+  }
 
   signOut() {
     this.auth.signOut();
