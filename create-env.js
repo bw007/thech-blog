14e463e9883ace47468d46c0b767273611a86e52
bw007 <mr.khasanov02@gmail.com>
2026-03-29 07:02:14 +0500

    generate environment from netlify env vars

---
+++ create-env.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const env = `export const environment = {
+  production: true,
+  supabase: {
+    api_url: '${process.env.SUPABASE_URL}',
+    api_key: '${process.env.SUPABASE_KEY}'
+  },
+  telegram: {
+    bot_token: '${process.env.TELEGRAM_BOT_TOKEN}'
+  }
+};
+`;
+
+fs.mkdirSync('./src/environments', { recursive: true });
+fs.writeFileSync('./src/environments/environment.ts', env);
+console.log('✅ environment.ts created');
