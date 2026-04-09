13c7257f929de92fe8c68a71a66706f83fba0e79
bw007 <mr.khasanov02@gmail.com>
2026-04-09 23:51:10 +0500

    refactor: move article detail component

---
+++ src/app/features/articles/article-detail/article-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { TuiButton, TuiIcon } from "@taiga-ui/core";
+import type { Article } from '@core/models/article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article-detail.component.html',
+  imports: [
+    RouterLink,
+    TuiButton,
+    TuiIcon,
+    DatePipe
+  ],
+})
+export class ArticleComponent {
+  private readonly route = inject(ActivatedRoute);
+
+  protected readonly article = this.route.snapshot.data['article'] as Article;
+}
