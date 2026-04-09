f39b562ceeb3a8d3cd1a2ff6c910494e348eefa0
bw007 <mr.khasanov02@gmail.com>
2026-04-09 23:26:13 +0500

    feat: implement published articles, queries for author data

--- src/app/core/services/article.service.ts
+++ src/app/core/services/article.service.ts
@@ -2,17 +2,15 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable, signal } from "@angular/core";
 import { Article } from "@core/models/article.model";
 import { finalize, map, tap } from "rxjs";
-import { AuthService } from "./auth.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ArticleService {
   private http = inject(HttpClient);
-  private auth = inject(AuthService);
 
-  private _mockArticles = signal<Article[]>([]);
-  latestArticles = this._mockArticles.asReadonly();
+  private _publishedArticles = signal<Article[]>([]);
+  publishedArticles = this._publishedArticles.asReadonly();
   
   private _userArticles = signal<Article[]>([]);
   userArticles = this._userArticles.asReadonly();
@@ -22,29 +20,48 @@ export class ArticleService {
 
   setActionLoadingId(id: string | null) {
     this._actionLoadingId.set(id);
-  }
+  };
 
+  // Get published articles
+  getPublishedArticles() {
+    return this.http.get<Article[]>(
+      `articles?status=eq.published&select=*,author:profiles(username,full_name,avatar_url)`
+    ).pipe(
+      tap((articles) => this._publishedArticles.set(articles))
+    );
+  };
+
+  // Get article by id
   getArticleById(id: string) {
     return this.http.get<Article[]>(`articles?id=eq.${id}`).pipe(
       map((articles) => articles[0])
     );
   };
 
+  // Get user articles
   getUserArticles(userId: string) {
-    return this.http.get<Article[]>(`articles?author_id=eq.${userId}&status=eq.published`)
-      .pipe(
-        tap((articles) => this._userArticles.set(articles))
-      );
+    return this.http.get<Article[]>(
+      `articles?author_id=eq.${userId}&status=eq.published&select=*,author:profiles(username,full_name,avatar_url)`
+    ).pipe(
+      tap((articles) => this._userArticles.set(articles))
+    );
   };
 
   // Publish article
   publishArticle(id: string) {
     this._actionLoadingId.set(id);
-    return this.http.patch<Article[]>(`articles?id=eq.${id}&select=*,author:profiles(username,full_name,avatar_url)`, {
-      status: 'published',
-      published_at: new Date().toISOString()
-    }).pipe(
-      map((articles) => articles[0]),
+    return this.http.patch<Article[]>(
+      `articles?id=eq.${id}&select=*,author:profiles(username,full_name,avatar_url)`,
+      {
+        status: 'published',
+        published_at: new Date().toISOString()
+      }
+    ).pipe(
+      map((articles) => {
+        const updatedArticle = articles[0];
+        this._userArticles.update((articles) => articles.map((a) => a.id === updatedArticle.id ? updatedArticle : a));
+        return updatedArticle;
+      }),
       finalize(() => this._actionLoadingId.set(null))
     )
   };
@@ -52,11 +69,18 @@ export class ArticleService {
   // Unpublish article
   unpublishArticle(id: string) {
     this._actionLoadingId.set(id);
-    return this.http.patch<Article[]>(`articles?id=eq.${id}&select=*,author:profiles(username,full_name,avatar_url)`, {
-      status: 'draft',
-      published_at: null
-    }).pipe(
-      map((articles) => articles[0]),
+    return this.http.patch<Article[]>(
+      `articles?id=eq.${id}&select=*,author:profiles(username,full_name,avatar_url)`,
+      {
+        status: 'draft',
+        published_at: null
+      }
+    ).pipe(
+      map((articles) => {
+        const updatedArticle = articles[0];
+        this._userArticles.update((articles) => articles.map((a) => a.id === updatedArticle.id ? updatedArticle : a));
+        return updatedArticle;
+      }),
       finalize(() => this._actionLoadingId.set(null))
     )
   };
@@ -64,8 +88,14 @@ export class ArticleService {
   // Delete article
   deleteArticle(id: string) {
     this._actionLoadingId.set(id);
-    return this.http.delete<Article[]>(`articles?id=eq.${id}&select=*,author:profiles(username,full_name,avatar_url)`).pipe(
-      map((articles) => articles[0]),
+    return this.http.delete<Article[]>(
+      `articles?id=eq.${id}&select=*,author:profiles(username,full_name,avatar_url)`
+    ).pipe(
+      map((articles) => {
+        const updatedArticle = articles[0];
+        this._userArticles.update((articles) => articles.filter((a) => a.id !== updatedArticle.id));
+        return updatedArticle;
+      }),
       finalize(() => this._actionLoadingId.set(null))
     )
   };
