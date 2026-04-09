def07808d8ea9eeeff0b774e5c6459b523dd7772
bw007 <mr.khasanov02@gmail.com>
2026-04-09 22:19:31 +0500

    feat: implement reusable article-card component

---
+++ src/app/shared/components/article-card/article-card.component.ts
@@ -0,0 +1,36 @@
+import { Component, computed, inject, input, output, signal } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { DatePipe } from "@angular/common";
+import { TuiIcon, TuiDataList, TuiDropdown, TuiButton } from "@taiga-ui/core";
+import { TuiChip } from "@taiga-ui/kit";
+import type { Article } from "@core/models/article.model";
+import { AuthService } from "@core/services/auth.service";
+
+@Component({
+  selector: 'app-article-card',
+  templateUrl: './article-card.component.html',
+  imports: [
+    RouterLink,
+    DatePipe,
+    TuiIcon,
+    TuiDataList,
+    TuiDropdown,
+    TuiChip,
+    TuiButton
+  ],
+})
+export class ArticleCardComponent {
+  private readonly auth = inject(AuthService);
+
+  article = input.required<Article>();
+  loadingId = input<string | null>(null);
+  editArticle = output<string>();
+  publishArticle = output<string>();
+  unpublishArticle = output<string>();
+  deleteArticle = output<string>();
+
+  readonly isMyArticle = computed(() => {
+    return this.auth.currentUser()?.id === this.article().author_id;
+  });
+  readonly dropdownOpen = signal(false);
+}
