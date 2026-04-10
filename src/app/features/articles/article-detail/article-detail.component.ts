f846a0d067bac0c2718aab68cd08357e091d12fa
bw007 <mr.khasanov02@gmail.com>
2026-04-10 12:36:33 +0500

    style: update article detail component, implement modern design

--- src/app/features/articles/article-detail/article-detail.component.ts
+++ src/app/features/articles/article-detail/article-detail.component.ts
@@ -1,8 +1,14 @@
 import { Component, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { DatePipe } from '@angular/common';
+import { generateHTML } from '@tiptap/core'
+import { DatePipe, Location } from '@angular/common';
 import { TuiButton, TuiIcon } from "@taiga-ui/core";
+import { TuiChip } from "@taiga-ui/kit";
 import type { Article } from '@core/models/article.model';
+import StarterKit from '@tiptap/starter-kit';
+import Image from '@tiptap/extension-image';
+import { Figcaption, Figure } from '@features/editor/extensions/figure.extension';
 
 @Component({
   selector: 'app-article',
@@ -11,11 +17,24 @@ import type { Article } from '@core/models/article.model';
     RouterLink,
     TuiButton,
     TuiIcon,
-    DatePipe
-  ],
+    DatePipe,
+    TuiChip
+],
 })
 export class ArticleComponent {
   private readonly route = inject(ActivatedRoute);
+  private readonly location = inject(Location);
+  private sanitizer = inject(DomSanitizer);
 
   protected readonly article = this.route.snapshot.data['article'] as Article;
+  protected readonly articleContent = this.article ? this.sanitizer.bypassSecurityTrustHtml(generateHTML(this.article.content, [
+    StarterKit,
+    Image,
+    Figure,
+    Figcaption,
+  ])) : null;
+
+  protected back() {
+    this.location.back();
+  }
 }
