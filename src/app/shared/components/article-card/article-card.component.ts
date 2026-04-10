68ec5bc6fa322431a423430384992705618c9fe9
bw007 <mr.khasanov02@gmail.com>
2026-04-10 12:43:53 +0500

    feat: parsing json to html

--- src/app/shared/components/article-card/article-card.component.ts
+++ src/app/shared/components/article-card/article-card.component.ts
@@ -1,10 +1,15 @@
 import { Component, computed, inject, input, output, signal } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { DomSanitizer } from "@angular/platform-browser";
 import { DatePipe } from "@angular/common";
 import { TuiIcon, TuiDataList, TuiDropdown, TuiButton } from "@taiga-ui/core";
 import { TuiChip } from "@taiga-ui/kit";
 import type { Article } from "@core/models/article.model";
 import { AuthService } from "@core/services/auth.service";
+import { generateHTML } from "@tiptap/core";
+import StarterKit from "@tiptap/starter-kit";
+import Image from "@tiptap/extension-image";
+import { Figcaption, Figure } from "@features/editor/extensions/figure.extension";
 
 @Component({
   selector: 'app-article-card',
@@ -21,6 +26,7 @@ import { AuthService } from "@core/services/auth.service";
 })
 export class ArticleCardComponent {
   private readonly auth = inject(AuthService);
+  private readonly sanitizer = inject(DomSanitizer);
 
   article = input.required<Article>();
   loadingId = input<string | null>(null);
@@ -34,4 +40,13 @@ export class ArticleCardComponent {
     return this.auth.currentUser()?.id === this.article().author_id;
   });
   readonly dropdownOpen = signal(false);
+
+  protected getArticleContent(content: Article['content']) {
+    return this.sanitizer.bypassSecurityTrustHtml(generateHTML(content, [
+      StarterKit,
+      Image,
+      Figure,
+      Figcaption,
+    ]))
+  }
 }
