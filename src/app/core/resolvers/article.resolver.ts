40de79d807bd70c1168887967aa71460316e0fd5
bw007 <mr.khasanov02@gmail.com>
2026-05-18 00:43:23 +0500

    feat: add line progress bar

--- src/app/core/resolvers/article.resolver.ts
+++ src/app/core/resolvers/article.resolver.ts
@@ -1,19 +1,24 @@
 import { inject } from "@angular/core";
-import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
-import { catchError, of } from "rxjs";
+import { catchError, finalize, of } from "rxjs";
+import type { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
 import type { Article } from "@core/models/article.model";
 import { ArticleService } from "@core/services/article.service";
+import { ProgressService } from "@core/services/progress.service";
 
 export const articleResolver: ResolveFn<Article | null> = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
+  const progress = inject(ProgressService);
   const article = inject(ArticleService);
   
   const articleId = route.paramMap.get('id');
   if (!articleId) return null;
 
+  progress.start();
+
   return article.getArticleById(articleId).pipe(
-    catchError(() => of(null))
+    catchError(() => of(null)),
+    finalize(() => progress.finish())
   );
 }
--- src/app/core/resolvers/editor.resolver.ts
+++ src/app/core/resolvers/editor.resolver.ts
@@ -2,15 +2,23 @@ import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
 import { EditorService } from "@core/services/editor.service";
 import type { Article } from "@core/models/article.model";
+import { catchError, finalize, of } from "rxjs";
+import { ProgressService } from "@core/services/progress.service";
 
 export const editorResolver: ResolveFn<Article | null> = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
+  const progress = inject(ProgressService);
   const editor = inject(EditorService);
   
   const articleId = route.paramMap.get('id');
   if (!articleId) return null;
 
-  return editor.getDraftArticle(articleId);
+  progress.start();
+
+  return editor.getDraftArticle(articleId).pipe(
+    catchError(() => of(null)),
+    finalize(() => progress.finish())
+  );
 }
