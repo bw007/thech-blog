4419278016e58af395aba7e3d419d6b1debcdb75
bw007 <mr.khasanov02@gmail.com>
2026-05-17 23:58:25 +0500

    update

--- src/app/shared/ui/progress-bar.component.ts
+++ src/app/shared/ui/progress-bar.component.ts
@@ -1,9 +1,8 @@
-import { Component, inject, input, PLATFORM_ID } from "@angular/core";
-import { TuiProgress, TuiProgressBar } from "@taiga-ui/kit";
-import {  } from '@ng-web-apis/platform';
-import { AsyncPipe, isPlatformServer } from "@angular/common";
-import { map, Observable, of, startWith, timer } from "rxjs";
+import { Component, inject, input } from "@angular/core";
+import { TuiProgress } from "@taiga-ui/kit";
+import { AsyncPipe } from "@angular/common";
 import { TuiSizeS, TuiSizeXS, TuiSizeXXS } from "@taiga-ui/core";
+import { ProgressService } from "@core/services/progress.service";
 
 @Component({
   selector: 'app-progress-bar',
@@ -13,11 +12,14 @@ import { TuiSizeS, TuiSizeXS, TuiSizeXXS } from "@taiga-ui/core";
     color="var(--color-green-300)"
     class="bg-transparent!"
     tuiProgressBar
-    [value]="value() | async"
+    [value]="value | async"
   ></progress>`,
   imports: [ AsyncPipe, TuiProgress]
 })
 export class ProgressBarComponent {
-  value = input<Observable<string | number>>();
+  private progressService = inject(ProgressService);
+
+  protected value = this.progressService.progressPercentage$;
+  // value = input<Observable<string | number>>();
   size = input<TuiSizeS | TuiSizeXXS | TuiSizeXS>('xxs')
 }
