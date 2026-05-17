b5106013ff388a730da2b91ffc21afa1fe0fa416
bw007 <mr.khasanov02@gmail.com>
2026-05-17 23:56:51 +0500

    feat: add line progress bar

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,12 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponnet } from "./footer/footer.component";
+import { ProgressBarComponent } from "@shared/ui/progress-bar.component";
+import { ArticleService } from "@core/services/article.service";
+import { ProgressService } from "@core/services/progress.service";
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  imports: [RouterOutlet, HeaderComponent, FooterComponnet]
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponnet,
+    ProgressBarComponent
+  ]
 })
 
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  private progressService = inject(ProgressService);
+  private article = inject(ArticleService);
+}
