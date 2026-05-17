ed4010c2ac7832e50989220f59dca4becf4842c0
bw007 <mr.khasanov02@gmail.com>
2026-05-18 00:43:16 +0500

    feat: add line progress bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,15 @@ import { TuiRoot } from "@taiga-ui/core";
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from "@core/services/theme.service";
+import { ProgressBarComponent } from "@shared/ui/progress-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, ProgressBarComponent],
   template: `
     <tui-root [attr.tuiTheme]="darkMode() ? 'dark' : 'light'">
       <router-outlet />
+      <app-progress-bar />
     </tui-root>
   `,
   styles: [],
